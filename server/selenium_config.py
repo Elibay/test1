760f896ee6a9d6a46a2f986134cd928ab1f7d41a
PhenHF <112208382+PhenHF@users.noreply.github.com>
2023-08-19 15:40:39 +0300

    Update selenium_config.py
    asf

--- server/selenium_config.py
+++ server/selenium_config.py
@@ -9,6 +9,6 @@ class webBrowser:
         self._options.page_load_strategy = 'eager'
         self._options.add_argument('--headless')
         self._options.add_argument('--no-sandbox')
-        self._service = webdriver.ChromeService(executable_path=path)
+        self._service = webdriver.ChromeService(executable_path='chromedriver/chromedriver')
         self.browser = webdriver.Chrome(service=self._service, options=self._options)
         self.browser.maximize_window()
