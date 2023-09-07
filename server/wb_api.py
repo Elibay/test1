1b74950ed09c9fef246bc0fa2fa97661c20e3bba
Vladimir_Makarov <phenloveu@gmail.com>
2023-09-07 13:48:59 +0300

    check2

--- server/wb_api.py
+++ server/wb_api.py
@@ -12,7 +12,8 @@ def post_price(nm_price, cof, category):
     data = []
     for i in range(len(nm_price)):
         if nm_price[i][0] != None:
-            data_dict = {'nmId': int(str(nm_price[i][1]).replace(".", "")), 'price' : ((float(str(cof[i][0]).replace(",","."))*(int(nm_price[i][0])+700)+(int(nm_price[i][0])+700)))/1-float(str(category[i][0]).replace(',', '.'))//get_rate()}
+            data_dict = {'nmId': int(str(nm_price[i][1]).replace(".", "")), 'price' : ((float(str(cof[i][0]).replace(",","."))*(int(nm_price[i][0])+700)+(int(nm_price[i][0])+700)))/float((1-float(str(category[i][0]).replace(",", "."))))//get_rate()}
+            #((float(str(cof[i][0]).replace(",","."))*(int(nm_price[i][0])+700)+(int(nm_price[i][0])+700)))/float((1-float(str(category[i][0]).replace(",", "."))))//get_rate())
             data.append(data_dict)
     print(data_dict['price'])
     res = requests.post(url='https://suppliers-api.wildberries.ru/public/api/v1/prices', headers=headers, json=data)
